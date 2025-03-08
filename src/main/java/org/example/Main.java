a2eb9254d1ac782e0e48c66f9aa9c8686aebb4e3
Ivan Vansos <vannosiska@gmail.com>
2025-03-08 22:37:17 +0700

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HI");
+        batton batton = new batton();
+        System.out.println("New click: " + batton.new_click());
+        System.out.println("One more click: " + batton.new_click());
     }
 }
---
+++ src/main/java/org/example/batton.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class batton {
+    private int click = 0;
+    public int new_click()
+    {
+        click++;
+        return click;
+    }
+
+}
